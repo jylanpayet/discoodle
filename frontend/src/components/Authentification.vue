4a5053c01e513252b3fd7806e19092f659a42c76
bastiansmn <bastian.somon@gmail.com>
2021-03-12 20:49:17 +0100

    Début de la mise en place de la nouvelle configuration maven. (2)

--- frontend/src/components/Authentification.vue
+++ frontend/src/components/Authentification.vue
@@ -87,197 +87,176 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .container {
-   position: absolute;
-   top: 25%;
-   left: 30%;
-   width: 768px;
-   height: 480px;
-   border-radius: 10px;
-   overflow: hidden;
-   box-shadow: 0 15px 30px rgba(0, 0, 0, .2),
-   0 10px 10px rgba(0, 0, 0, .2);
-   background: linear-gradient(to bottom, #ffffff, #f4f4f4);
-
-   .overlay-container {
-      position: absolute;
-      top: 0;
-      left: 50%;
-      width: 50%;
-      height: 100%;
-      overflow: hidden;
-      transition: transform .5s ease-in-out;
-      z-index: 100;
-   }
-
-   .overlay {
-      position: relative;
-      left: -100%;
-      height: 100%;
-      width: 200%;
-      background: linear-gradient(to bottom right, #e85c5c, #e86c5c);
-      color: #fff;
-      transform: translateX(0);
-      transition: transform .5s ease-in-out;
-   }
-
-   @mixin overlays($property) {
-      position: absolute;
-      top: 0;
-      display: flex;
-      align-items: center;
-      justify-content: space-around;
-      flex-direction: column;
-      padding: 70px 40px;
-      width: calc(50% - 80px);
-      height: calc(100% - 140px);
-      text-align: center;
-      transform: translateX($property);
-      transition: transform .5s ease-in-out;
-   }
-
-   .overlay-left {
-      @include overlays(-20%);
-   }
-
-   .overlay-right {
-      @include overlays(0);
-      right: 0;
-   }
+  position: absolute;
+  top: 25%;
+  left: 30%;
+  width: 768px;
+  height: 480px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, .2), 0 10px 10px rgba(0, 0, 0, .2);
+  background: linear-gradient(to bottom, #fff, #f4f4f4);
 }
-
+.container .overlay-container {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  transition: transform 0.5s ease-in-out;
+  z-index: 100;
+}
+.container .overlay {
+  position: relative;
+  left: -100%;
+  height: 100%;
+  width: 200%;
+  background: linear-gradient(to bottom right, #e85c5c, #e86c5c);
+  color: #fff;
+  transform: translateX(0);
+  transition: transform 0.5s ease-in-out;
+}
+.container .overlay-left {
+  position: absolute;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 70px 40px;
+  width: calc(50% - 80px);
+  height: calc(100% - 140px);
+  text-align: center;
+  transform: translateX(-20%);
+  transition: transform 0.5s ease-in-out;
+}
+.container .overlay-right {
+  position: absolute;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 70px 40px;
+  width: calc(50% - 80px);
+  height: calc(100% - 140px);
+  text-align: center;
+  transform: translateX(0);
+  transition: transform 0.5s ease-in-out;
+  right: 0;
+}
 h2 {
-   margin: 0;
+  margin: 0;
 }
-
 p {
-   margin: 20px 0 30px;
+  margin: 20px 0 30px;
 }
-
 a {
-   color: #222;
-   text-decoration: none;
-   margin: 15px 0;
-   font-size: 1rem;
+  color: #222;
+  text-decoration: none;
+  margin: 15px 0;
+  font-size: 1rem;
 }
-
 button {
-   border-radius: 20px;
-   border: 1px solid #e85c5c;
-   background-color: #e85c5c;
-   color: #fff;
-   font-size: 1rem;
-   font-weight: bold;
-   padding: 10px 40px;
-   letter-spacing: 1px;
-   text-transform: uppercase;
-   cursor: pointer;
-   transition: transform .1s ease-in;
-
-   &:active {
-      transform: scale(.9);
-   }
-
-   &:focus {
-      outline: none;
-   }
+  border-radius: 20px;
+  border: 1px solid #e85c5c;
+  background-color: #e85c5c;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 10px 40px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: transform 0.1s ease-in;
 }
-
+button:active {
+  transform: scale(0.9);
+}
+button:focus {
+  outline: none;
+}
 button.invert {
-   background-color: transparent;
-   border-color: #fff;
+  background-color: transparent;
+  border-color: #fff;
 }
-
 .sign-up, .sign-in {
-   position: absolute;
-   top: 0;
-   display: flex;
-   align-items: center;
-   justify-content: space-around;
-   flex-direction: column;
-   padding: 90px 60px;
-   width: calc(50% - 120px);
-   height: calc(100% - 180px);
-   text-align: center;
-   background: linear-gradient(to bottom, #ffffff, #f4f4f4);
-   transition: all .5s ease-in-out;
-
-   div {
-      font-size: 1rem;
-   }
-
-   input {
-      background-color: #eee;
-      border: none;
-      padding: 8px 15px;
-      margin: 6px 0;
-      width: calc(100% - 30px);
-      border-radius: 15px;
-      border-bottom: 1px solid #ddd,
-      0 -1px 1px #fff,
-      0 1px 0 #fff;
-      overflow: hidden;
-
-      &:focus {
-         outline: none;
-         background-color: #fff;
-      }
-   }
+  position: absolute;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 90px 60px;
+  width: calc(50% - 120px);
+  height: calc(100% - 180px);
+  text-align: center;
+  background: linear-gradient(to bottom, #fff, #f4f4f4);
+  transition: all 0.5s ease-in-out;
 }
-
+.sign-up div, .sign-in div {
+  font-size: 1rem;
+}
+.sign-up input, .sign-in input {
+  background-color: #eee;
+  border: none;
+  padding: 8px 15px;
+  margin: 6px 0;
+  width: calc(100% - 30px);
+  border-radius: 15px;
+  border-bottom: 1px solid #ddd;
+  overflow: hidden;
+}
+.sign-up input:focus, .sign-in input:focus {
+  outline: none;
+  background-color: #fff;
+}
 .sign-in {
-   left: 0;
-   z-index: 2;
+  left: 0;
+  z-index: 2;
 }
-
 .sign-up {
-   left: 0;
-   z-index: 1;
-   opacity: 0;
+  left: 0;
+  z-index: 1;
+  opacity: 0;
 }
-
-.sign-up-active {
-   .sign-in {
-      transform: translateX(100%);
-   }
-
-   .sign-up {
-      transform: translateX(100%);
-      opacity: 1;
-      z-index: 5;
-      animation: show .5s;
-   }
-
-   .overlay-container {
-      transform: translateX(-100%);
-   }
-
-   .overlay {
-      transform: translateX(50%);
-   }
-
-   .overlay-left {
-      transform: translateX(0);
-   }
-
-   .overlay-right {
-      transform: translateX(20%);
-   }
+.sign-up-active .sign-in {
+  transform: translateX(100%);
 }
-
+.sign-up-active .sign-up {
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
+  animation: show 0.5s;
+}
+.sign-up-active .overlay-container {
+  transform: translateX(-100%);
+}
+.sign-up-active .overlay {
+  transform: translateX(50%);
+}
+.sign-up-active .overlay-left {
+  transform: translateX(0);
+}
+.sign-up-active .overlay-right {
+  transform: translateX(20%);
+}
 @keyframes show {
-   0% {
-      opacity: 0;
-      z-index: 1;
-   }
-   49% {
-      opacity: 0;
-      z-index: 1;
-   }
-   50% {
-      opacity: 1;
-      z-index: 10;
-   }
+  0% {
+    opacity: 0;
+    z-index: 1;
+  }
+  49% {
+    opacity: 0;
+    z-index: 1;
+  }
+  50% {
+    opacity: 1;
+    z-index: 10;
+  }
 }
+
 </style>
