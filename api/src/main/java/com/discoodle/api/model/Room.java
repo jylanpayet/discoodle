9e2752b872aee6fecf52817bb24d1d0d1ec376f6
dylan <dylan_alexandre@outlook.fr>
2021-03-12 01:10:52 +0100

    Implémentation des premières tables pour la structure des rooms ainsi que les premières fonctions afin d'insérer une conversation qui vient d'être créée dans la BDD avec les informations à retenir.

---
+++ api/src/main/java/com/discoodle/api/model/Room.java
@@ -0,0 +1,40 @@
+package com.discoodle.api.model;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@ToString
+@Table(name = "room")
+public class Room {
+
+    public Room(String name, Integer user_ID) {
+        this.name = name;
+        this.user_ID = user_ID;
+    }
+
+    @Id
+    /*@GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")*/
+    @GeneratedValue
+    @Column(name = "room_id", unique = true, nullable = false)
+    private Integer uuid;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "admin")
+    private Integer user_ID;
+    @Column(name = "link_picture")
+    private String link_picture;
+
+    @ManyToMany
+    @JoinTable( name = "link_rooms_users",
+            joinColumns = @JoinColumn( name = "room_id" ),
+            inverseJoinColumns = @JoinColumn( name = "user_id" ) )
+    private List<User> users = new ArrayList<>();
+}
