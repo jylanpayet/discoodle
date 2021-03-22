abe71a568a6047d99cfc5183ab0d67db2d2d3f70
CLIPAL moise <elymo.mc@gmail.com>
2021-03-22 23:14:09 +0100

    Classe pour gerer les droits.

--- api/src/main/java/com/discoodle/api/model/Group.java
+++ api/src/main/java/com/discoodle/api/model/Group.java
@@ -1,6 +1,5 @@
 package com.discoodle.api.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,21 +11,23 @@ import java.util.List;
 @Setter
 public class Group {
 
-    Integer id;
-    Group parent;
-    String name;
-    Integer depth;
-    List<Group> underGroups;
-    List<User> users;
-    String usersGroupName;
+    private Integer id;
+    private Group parent;
+    private String name;
+    private Integer depth;
+    private List<Group> underGroups;
+    private List<User> users;
+    private String usersGroupName;
+    private GroupRights groupRights;
 
-    public Group(Group parent, String name, Integer depth, List<Group> underGroups, List<User> users, String usersGroupName) {
+    public Group(Group parent, String name, Integer depth, List<Group> underGroups, List<User> users, String usersGroupName, GroupRights groupRights) {
         this.parent = parent;
         this.name = name;
         this.depth = depth;
         this.underGroups = underGroups;
         this.users = users;
         this.usersGroupName = usersGroupName;
+        this.groupRights = groupRights;
     }
 
 }
---
+++ api/src/main/java/com/discoodle/api/model/GroupRights.java
@@ -0,0 +1,22 @@
+package com.discoodle.api.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class GroupRights {
+
+    private Integer id;
+    private boolean canDeleteUser;
+    private boolean canAddUser;
+
+    public GroupRights(boolean canDeleteUser, boolean canAddUser) {
+        this.canDeleteUser = canDeleteUser;
+        this.canAddUser = canAddUser;
+    }
+
+}
