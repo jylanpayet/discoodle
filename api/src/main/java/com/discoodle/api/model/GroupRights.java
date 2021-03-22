6efa6f18bfc81db58835b9dded2e05f3a1a14618
CLIPAL moise <elymo.mc@gmail.com>
2021-03-22 23:30:11 +0100

    Ajout des attributs de l'objet dans la base de données (bdd).

--- api/src/main/java/com/discoodle/api/model/GroupRights.java
+++ api/src/main/java/com/discoodle/api/model/GroupRights.java
@@ -3,21 +3,32 @@ package com.discoodle.api.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.persistence.*;
 
 
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "group_rights")
 public class GroupRights {
-
-    private Integer id;
-    private boolean canDeleteUser;
-    private boolean canAddUser;
 
     public GroupRights(boolean canDeleteUser, boolean canAddUser) {
         this.canDeleteUser = canDeleteUser;
         this.canAddUser = canAddUser;
     }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "rights_id", unique = true, nullable = false)
+    private Integer RightsId;
+
+    @Column(name = "can_delete_user")
+    private boolean canDeleteUser;
+
+    @Column(name = "can_add_user")
+    private boolean canAddUser;
+
 
 
 }
