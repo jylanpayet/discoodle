e35067e201d8dbb254419d58ce1ded59d16fe4c8
jylanpayet <jylanpayet@gmail.com>
2021-03-23 14:07:43 +0100

    Creation d'un repository pour intéragir avec un Groups.

---
+++ api/src/main/java/com/discoodle/api/repository/GroupsRepository.java
@@ -0,0 +1,41 @@
+package com.discoodle.api.repository;
+
+import com.discoodle.api.model.Groups;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+
+@Repository
+public interface GroupsRepository extends JpaRepository<Groups,Integer> {
+
+    @Query(value = "SELECT groups FROM Groups groups where groups.groups_id=?1")
+    Optional<Groups> findGroupsById(Integer id);
+
+    @Query(value = "SELECT groups FROM Groups groups where groups.name=?1")
+    Optional<Groups> findAllGroupsByName(String name);
+
+    @Query(value = "SELECT groups FROM Groups groups where groups.usersGroupName=?1")
+    Optional<Groups> findAllGroupsByUserGroupName(String userGroupName);
+
+    @Query(value = "SELECT groups FROM Groups groups where groups.depth=?1")
+    Optional<Groups> findAllGroupsByDepth(Integer depth);
+
+    // Just to have the rights of this group and not all information.
+    @Query(value = "SELECT GroupRights FROM Groups groups where groups.groups_id=?1")
+    Optional<Groups> findRightsInGroup(Integer groups_id);
+
+    @Modifying
+    @Query(value = "insert into link_groups_to_user (user_id, groups_id) VALUES (:user_id,:groups_id)", nativeQuery = true)
+    @Transactional
+    void addNewMemberInGroup(@Param("user_id") Integer user_ID, @Param("groups_id") Integer groups_ID);
+
+    @Modifying
+    @Query(value = "insert into link_groups_to_group (groups_id, son_id) VALUES (:groups_id,:son_id)", nativeQuery = true)
+    @Transactional
+    void addNewGroupsInGroup(@Param("groups_id") Integer groups_ID, @Param("son_id") Integer son_ID);
+}
