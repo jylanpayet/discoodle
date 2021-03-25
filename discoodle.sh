164e710806e6eada8b831831d7d7de1de204a218
Bastian SOMON <bastian.somon@gmail.com>
2021-03-25 22:20:42 +0100

    Fix d'un bug qui empechait le lancement de la configuration sur linux.

--- discoodle.sh
+++ discoodle.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo ""
 echo "  _____  _                         _ _
  |  __ \(_)                       | | |
@@ -7,31 +8,23 @@ echo "  _____  _                         _ _
  |_____/|_|___/\___\___/ \___/ \__,_|_|\___|
 
                                             "
-
-if [ $# == 1 ]
-then
+if [ $# == 1 ]; then
   echo -e "\033[32;1m[ATTENTION] N'éteignez pas le programme avec un Ctrl+c au risque de laisser le port 8080 actif !\033[m"
   sleep 2
   echo ""
   clear
-
   echo "Quel est votre mot de passe mariadb ? :"
   mariadb -u $1 -p -e 'create database if not exists discoodle'
-  if [ $? != 0 ]
-  then
+  if [ $? != 0 ]; then
     echo -e "\033[32;1m[ERREUR] Impossible d'exécuter le script SQL. Veuillez rééssayer.\033[m"
     exit
   fi
   cd api
-
   mvn spring-boot:run &
   sleep 15
-
   cd ../frontend
   npm run serve
-
-elif [ $# == 0 ]
-then
+elif [ $# == 0 ]; then
   echo -e "\033[31;1mVous n'avez pas entré votre nom d'utilisateur mariadb en argument du programme\033[m"
   echo ""
   echo "Veuillez rééssayer avec la commande suivante :"
