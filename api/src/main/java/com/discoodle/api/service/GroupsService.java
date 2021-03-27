879080b19903a86951542e3927fbf72a72d3e4ac
jylanpayet <jylanpayet@gmail.com>
2021-03-27 02:19:54 +0100

    Nouveau service afin d'éditer les droits d'un Groups

--- api/src/main/java/com/discoodle/api/service/GroupsService.java
+++ api/src/main/java/com/discoodle/api/service/GroupsService.java
@@ -1,6 +1,7 @@
 package com.discoodle.api.service;
 
 import com.discoodle.api.model.GroupRights;
+import com.discoodle.api.model.GroupRightsRequest;
 import com.discoodle.api.model.Groups;
 import com.discoodle.api.model.GroupsRequest;
 import com.discoodle.api.repository.GroupRightsRepository;
@@ -28,5 +29,17 @@ public class GroupsService {
         groupsRepository.addNewGroupsInGroup(request.getParent_id(),finalGroup.getGroups_id());
         groupsRepository.addNewMemberInGroup(request.getUser_id(),finalGroup.getGroups_id());
         return finalGroup;
+    }
+
+    public boolean editRights(GroupRightsRequest request){
+        GroupRights ofParent=groupsRepository.findGroupsById(groupsRepository.findParentOfGroup(request.getGroupId())).get().getGroupRights();
+        GroupRights r=groupsRepository.findGroupsById(request.getGroupId()).get().getGroupRights();
+        if(ofParent.isCanAddUser())
+            rightsRepository.updateRightsAdd(r.getRightsId(),request.isAddUser());
+        if(ofParent.isCanDeleteUser())
+            rightsRepository.updateRightsDelete(r.getRightsId(),request.isDeleteUser());
+        if(ofParent.isCanModify())
+            rightsRepository.updateRightsModify(r.getRightsId(),request.isModify());
+        return true;
     }
 }
