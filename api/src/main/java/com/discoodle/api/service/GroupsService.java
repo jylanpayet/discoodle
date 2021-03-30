91cd095c7aed58b7151efa3f3bbb0e801bbbf4fc
CLIPAL moise <elymo.mc@gmail.com>
2021-03-30 23:23:30 +0200

    Implémentation de fichier.json pour les cours

--- api/src/main/java/com/discoodle/api/service/GroupsService.java
+++ api/src/main/java/com/discoodle/api/service/GroupsService.java
@@ -1,5 +1,6 @@
 package com.discoodle.api.service;
 
+import com.discoodle.api.ApiApplication;
 import com.discoodle.api.model.GroupRights;
 import com.discoodle.api.request.EditGroupRequest;
 import com.discoodle.api.request.GroupRightsRequest;
@@ -10,6 +11,9 @@ import com.discoodle.api.repository.GroupsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
+
 @Service
 @AllArgsConstructor
 public class GroupsService {
@@ -17,7 +21,7 @@ public class GroupsService {
     private final GroupRightsRepository rightsRepository;
 
     public Groups createNewGroup(GroupsRequest request) {
-        GroupRights rights=new GroupRights(false, false, false);
+        GroupRights rights = new GroupRights(false, false, false);
         rights = rightsRepository.save(rights);
         Groups group = new Groups(
                 request.getDepth(),
@@ -26,13 +30,24 @@ public class GroupsService {
                 request.getType()
         );
         Groups finalGroup = groupsRepository.save(group);
-        groupsRepository.addNewRightsInGroup(finalGroup.getGroups_id(),rights.getRightsId());
-        groupsRepository.addNewGroupsInGroup(request.getParent_id(),finalGroup.getGroups_id());
-        groupsRepository.addNewMemberInGroup(request.getUser_id(),finalGroup.getGroups_id());
+        groupsRepository.addNewRightsInGroup(finalGroup.getGroups_id(), rights.getRightsId());
+        groupsRepository.addNewGroupsInGroup(request.getParent_id(), finalGroup.getGroups_id());
+        groupsRepository.addNewMemberInGroup(request.getUser_id(), finalGroup.getGroups_id());
+
+        if (finalGroup.getType().equals(Groups.TypeOfGroup.SUBJECTS)) {
+            try {
+                File dossier = new File((String.format("%sstatic/common/json/Groups/", ApiApplication.RESSOURCES)));
+                dossier.mkdirs();
+                File fichier = new File((String.format("%sstatic/common/json/Groups/%s.json", ApiApplication.RESSOURCES,finalGroup.getGroups_id())));
+                fichier.createNewFile();
+            } catch (Exception e){
+                System.out.println("Groups non crée !");
+            }
+        }
         return finalGroup;
     }
 
-    public boolean editRights(GroupRightsRequest request){
+    public boolean editRights(GroupRightsRequest request) {
         try {
             GroupRights ofParent = groupsRepository.findGroupsById(groupsRepository.findParentOfGroup(request.getGroupId())).get().getGroupRights();
             GroupRights r = groupsRepository.findGroupsById(request.getGroupId()).get().getGroupRights();
@@ -42,19 +57,21 @@ public class GroupsService {
                 rightsRepository.updateRightsDelete(r.getRightsId(), request.isDeleteUser());
             if (ofParent.isCanModify())
                 rightsRepository.updateRightsModify(r.getRightsId(), request.isModify());
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    public boolean editGroup(EditGroupRequest request){
-        try{
-           groupsRepository.updateNameAndDescGroup(request.getGroups_id(), request.getName(), request.getDescription());
-        }catch(Exception e){
+
+    public boolean editGroup(EditGroupRequest request) {
+        try {
+            groupsRepository.updateNameAndDescGroup(request.getGroups_id(), request.getName(), request.getDescription());
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
+
     public void deleteGroupByID(Integer groups_ID) {
         groupsRepository.deleteGroupsByID(groups_ID);
     }
