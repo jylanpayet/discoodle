dc05032a1f00691f23cb9cbb20e40bef7094e271
jylanpayet <jylanpayet@gmail.com>
2021-04-01 21:14:05 +0200

    Lister toutes les matières d'un user.

--- api/src/main/java/com/discoodle/api/controller/UserController.java
+++ api/src/main/java/com/discoodle/api/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.discoodle.api.controller;
 
+import com.discoodle.api.model.Groups;
 import com.discoodle.api.model.Room;
 import com.discoodle.api.model.User;
 import com.discoodle.api.service.UserService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,19 +22,19 @@ public class UserController {
     }
 
     @GetMapping
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @RequestMapping(value = "{username}", method = GET)
     @ResponseBody
-    public Optional<User> getUserByUserName(@PathVariable String username){
+    public Optional<User> getUserByUserName(@PathVariable String username) {
         return userService.getUserByUserName(username);
     }
 
     @RequestMapping(value = "/infos/{user_id}", method = GET)
     @ResponseBody
-    public Optional<User> getUserByID(@PathVariable Integer user_id){
+    public Optional<User> getUserByID(@PathVariable Integer user_id) {
         return userService.getUserByID(user_id);
     }
 
@@ -51,5 +52,18 @@ public class UserController {
     @ResponseBody
     public List<Room> findAllRoomsByUserID(@PathVariable Integer user_ID) {
         return userService.getUserByID(user_ID).get().getRooms();
+    }
+
+    @GetMapping(path = "/seeAllSubjects/{user_ID}")
+    @ResponseBody
+    public List<Groups> findAllSubjectsByUserID(@PathVariable Integer user_ID) {
+        List<Groups> AllSubjects = userService.getUserByID(user_ID).get().getGroups();
+        List<Groups> UserSubjects = new ArrayList<>();
+        for (int i = 0; i < AllSubjects.size(); i++) {
+            if (AllSubjects.get(i).getType().equals(Groups.TypeOfGroup.SUBJECTS)) {
+                UserSubjects.add(AllSubjects.get(i));
+            }
+        }
+        return UserSubjects;
     }
 }
