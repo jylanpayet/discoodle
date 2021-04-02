d91a9f4693756dcce75d10a7db6da520b21de680
dylan <dylan_alexandre@outlook.fr>
2021-04-02 10:54:40 +0200

    Fix bug pour l'attribut id d'un user.

--- api/src/main/java/com/discoodle/api/model/User.java
+++ api/src/main/java/com/discoodle/api/model/User.java
@@ -27,7 +27,7 @@ public class User {
     @Id
     @GeneratedValue
     @Column(name = "user_id", unique = true, nullable = false)
-    private Long user_id;
+    private Long id;
 
     @Column(name = "mail")
     private String mail;
