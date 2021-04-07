70bde8b8ec8e876f7cac1de356e15f6690756a03
jylanpayet <jylanpayet@gmail.com>
2021-04-08 01:44:24 +0200

    Erreur de PostMapping

--- api/src/main/java/com/discoodle/api/controller/ServerController.java
+++ api/src/main/java/com/discoodle/api/controller/ServerController.java
@@ -13,12 +13,12 @@ public class ServerController {
 
     //The server is generated automaticaly during the creation of a groups.SUBJECTS.
 
-    @PostMapping(path = "/api/server/{server_id}/server.add")
+    @PostMapping(path = "/api/server/{server_id}/server.addMember")
     public void addNewMember(@PathVariable Long server_id, @RequestParam(value = "user_id") Long user_id){
         serverService.addNewMember(server_id,user_id);
     }
 
-    @PostMapping(path = "/api/server/{server_id}/server.add")
+    @PostMapping(path = "/api/server/{server_id}/server.addRoom")
     public void addNewRoom(@PathVariable Long server_id,@RequestParam(value="name") String name){
         serverService.addNewRoom(server_id,name);
     }
