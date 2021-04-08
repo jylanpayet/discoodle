7711fa9c8ed02f83212ca887077c23d2eb29b52b
CLIPAL moise <elymo.mc@gmail.com>
2021-04-08 11:51:23 +0200

    Création d'un model pour la structure des Notes.

---
+++ api/src/main/java/com/discoodle/api/model/Note.java
@@ -0,0 +1,27 @@
+package com.discoodle.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Note {
+
+    private final long user_id;
+    private String user_firstName;
+    private String user_lastName;
+    private final Long group_id;
+    private double note;
+    private double coef;
+
+    public Note(long user_id, String user_firstName, String user_lastName, Long group_id, double note, double coef) {
+        this.user_id = user_id;
+        this.user_firstName = user_firstName;
+        this.user_lastName = user_lastName;
+        this.group_id = group_id;
+        this.note = note;
+        this.coef = coef;
+    }
+
+
+}
