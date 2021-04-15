5ce2b9ac6984637a201a8b39903b91bbe04d9ec5
jylanpayet <jylanpayet@gmail.com>
2021-04-15 14:08:55 +0200

    Vue de l'upload de fichiers actuellement mis dans planning, elle est modifiable, ajout d'une limite de taille de fichiers.

---
+++ frontend/src/components/UploadFile.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="global">
+    <form ref="uploadForm" @submit.prevent="submit">
+      <input type="file" ref="upload" @change="onFileUpload()" class="form-control" required>
+      <input type="button" @click="startUpload" name="Upload" value=" Upload ">
+    </form>
+
+    <form ref="uploadSubjectForm" @submit.prevent="submit">
+      <input type="file" ref="uploadSubject" @change="onSubjectUpload()" class="form-control" required>
+      <input type="button" @click="startSubjectUpload" name="Upload" value=" Upload ">
+    </form>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+export default {
+  name:"UploadFile",
+  data: () =>({
+    formData: null
+  }),
+  methods:{
+    onFileUpload(){
+      let file=this.$refs.upload.files[0];
+      this.formData=new FormData();
+      this.formData.append("file",file);
+    },
+    onSubjectUpload(){
+      let file=this.$refs.uploadSubject.files[0];
+      this.formData=new FormData();
+      this.formData.append("file",file);
+    },
+    startUpload() {
+      axios({
+        url: `http://localhost:8080/api/upload/1007`,
+        method: 'POST',
+        data: this.formData,
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'multipart/form-data'
+        }
+      }).then(response =>{
+        console.log(JSON.stringify(response.data))
+      });
+    },
+    startSubjectUpload() {
+      axios({
+        url: `http://localhost:8080/api/uploadSubject/1007`,
+        method: 'POST',
+        data: this.formData,
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'multipart/form-data'
+        }
+      }).then(response =>{
+        console.log(JSON.stringify(response.data))
+      });
+    }
+  }
+}
+</script>
