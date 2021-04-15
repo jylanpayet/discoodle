ec13af92f61f567941db12bb41aac7791400aeeb
jylanpayet <jylanpayet@gmail.com>
2021-04-15 14:05:56 +0200

    Fonction qui upload un fichier exclusivement en .md pour les cours.

--- api/src/main/java/com/discoodle/api/service/UploadFileService.java
+++ api/src/main/java/com/discoodle/api/service/UploadFileService.java
@@ -2,6 +2,7 @@ package com.discoodle.api.service;
 
 import com.discoodle.api.ApiApplication;
 import lombok.AllArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,4 +31,22 @@ public class UploadFileService {
         }
         return "Fichier upload avec succès !";
     }
+    public String uploadSubject(MultipartFile file, Long group_id) {
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        if (!extension.equals("md")) {
+            return "L'extension n'est pas un fichier md, il ne peut donc pas être upload";
+        }
+        String path = String.format("%sstatic/common/groups/%d/cours.md", ApiApplication.RESSOURCES, group_id);
+        File add = new File(path);
+        try {
+            if (!add.exists()) {
+                add.createNewFile();
+            }
+            Files.write(Path.of(path), file.getBytes());
+        } catch (Exception e) {
+            return "Erreur lors du téléchargement du fichier !";
+        }
+        return "Fichier upload avec succès !";
+    }
+}
 }
