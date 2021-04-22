1e483aaa4b5213d1c46c6287bdfcb50b37723a12
jylanpayet <jylanpayet@gmail.com>
2021-04-22 23:22:06 +0200

    Debug des requests.

--- api/src/main/java/com/discoodle/api/controller/UserController.java
+++ api/src/main/java/com/discoodle/api/controller/UserController.java
@@ -3,14 +3,13 @@ package com.discoodle.api.controller;
 import com.discoodle.api.model.Groups;
 import com.discoodle.api.model.Room;
 import com.discoodle.api.model.User;
+import com.discoodle.api.request.RegistrationRequest;
 import com.discoodle.api.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 @RequestMapping("api/users")
@@ -86,32 +85,32 @@ public class UserController {
     }
 
     @PostMapping("/changeUsername/{user_id}")
-    public Optional<User> changeUsername(@PathVariable Long user_id, @RequestBody User.RegistrationRequest request) {
+    public Optional<User> changeUsername(@PathVariable Long user_id, @RequestBody RegistrationRequest request) {
         return userService.changeUsername(user_id, request.getUsername());
     }
 
     @PostMapping("/changeMail/{user_id}")
-    public Optional<User> changeMail(@PathVariable Long user_id, @RequestBody User.RegistrationRequest request) {
+    public Optional<User> changeMail(@PathVariable Long user_id, @RequestBody RegistrationRequest request) {
         return userService.changeMail(user_id, request.getMail());
     }
 
     @PostMapping("/changePassword/{user_id}")
-    public Optional<User> changePassword(@PathVariable Long user_id, @RequestBody User.RegistrationRequest request) {
+    public Optional<User> changePassword(@PathVariable Long user_id, @RequestBody RegistrationRequest request) {
         return userService.changePassword(user_id, request.getPassword());
     }
 
     @PostMapping("/changeName/{user_id}")
-    public Optional<User> changeName(@PathVariable Long user_id, @RequestBody User.RegistrationRequest request) {
+    public Optional<User> changeName(@PathVariable Long user_id, @RequestBody RegistrationRequest request) {
         return userService.changeName(user_id, request.getName());
     }
 
     @PostMapping("/changeLastName/{user_id}")
-    public Optional<User> changeLastName(@PathVariable Long user_id, @RequestBody User.RegistrationRequest request) {
+    public Optional<User> changeLastName(@PathVariable Long user_id, @RequestBody RegistrationRequest request) {
         return userService.changeLastName(user_id, request.getLast_name());
     }
 
     @PostMapping("/changeLinkToAvar/{user_id}")
-    public Optional<User> changeLinkToAvatar(@PathVariable Long user_id, @RequestBody User.RegistrationRequest request) {
+    public Optional<User> changeLinkToAvatar(@PathVariable Long user_id, @RequestBody RegistrationRequest request) {
         return userService.changeLinkToAvatar(user_id, request.getLink_to_avatar());
     }
 
