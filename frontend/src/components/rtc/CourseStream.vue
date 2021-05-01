3f431d5fe9617e57250babeee0d919682bac014d
bastiansmn <bastian.somon@gmail.com>
2021-05-01 23:05:23 +0200

    Mise en place des composants du WEBRTC

---
+++ frontend/src/components/rtc/CourseStream.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="CourseStream">
+
+   </div>
+</template>
+
+<script>
+export default {
+   name: "CourseStream",
+   computed: {
+
+   },
+   methods: {
+
+   },
+   mounted() {
+
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
