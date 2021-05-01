e9abd13009d6dc2749745fd3e918ff9f07e1a144
jylanpayet <jylanpayet@gmail.com>
2021-05-01 16:50:05 +0200

    Un utilisateur ne peut plus être ajouter deux fois dans un même Server. Un utilisateur ajouter dans un groups obtient maintenant directement le role d'étudiant. Ajouter une nouvelle Room dans un Server la renvoie automatiquement.

--- api/src/main/java/com/discoodle/api/repository/RolesRepository.java
+++ api/src/main/java/com/discoodle/api/repository/RolesRepository.java
@@ -1,7 +1,16 @@
 package com.discoodle.api.repository;
 
+import com.discoodle.api.model.Message;
 import com.discoodle.api.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface RolesRepository extends JpaRepository<Roles, Long> {
+
+    @Query(value = "SELECT role FROM Roles role WHERE role.name = :name AND role.groups_id=:group_id ")
+    Optional<Roles> getRolesByNameAnAndGroupsId(String name, Long group_id);
+
 }
