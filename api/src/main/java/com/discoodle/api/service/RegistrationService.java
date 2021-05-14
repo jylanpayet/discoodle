50538febd7589abbe5440c0bcb2b5a1083a4e18a
dylan <dylan_alexandre@outlook.fr>
2021-05-14 02:11:21 +0200

    Ajout de commentaires sur les FriendshipsService et RegistrationService.

--- api/src/main/java/com/discoodle/api/service/RegistrationService.java
+++ api/src/main/java/com/discoodle/api/service/RegistrationService.java
@@ -23,9 +23,13 @@ public class RegistrationService {
     private final MailSender mailSender;
 
     public String register(RegistrationRequest request) {
-        if(userService.getUserByUserName(request.getUsername()).isEmpty() && userService.getUserByMail(request.getMail()).isEmpty()) {
+        // Check if username or/and mail entered exist already.
+        if(userService.getUserByUserName(request.getUsername()).isEmpty() || userService.getUserByMail(request.getMail()).isEmpty()) {
+            // Regex for password with at least 8 characters constitued from at least 1 number, 1 letter to lowercase, 1 letter to uppercase, 1 special character.
             if (request.getPassword().matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}")) {
+                // Regex for mail with at (@).
                 if (request.getMail().matches("^(.+)@(.+)$")) {
+                    // Create a token with user's inputs and add user in database.
                     String token = userService.signUpUser(
                             new User(
                                     request.getMail(),
@@ -36,6 +40,7 @@ public class RegistrationService {
                                     User.Role.STUDENT
                             )
                     );
+                    // Send mail verifcation.
                     /*String link = "http://localhost:8080/api/registration/confirm?token=" + token;
                     mailSender.send(request.getMail(),
                             buildMail(request.getName(), link));*/
@@ -65,29 +70,36 @@ public class RegistrationService {
 
     @Transactional
     public String confirmToken(String token, HttpServletResponse response) throws IOException {
+        // Try to find token if it exists in database.
         ConfirmationToken confirmationToken = confirmationTokenService
                 .getToken(token)
                 .orElseThrow(() ->
                         new IllegalStateException("Le token n'a pas été trouvé."));
 
+        // Check if token has been already confirmed.
         if (confirmationToken.getConfirmedAt() != null) {
             throw new IllegalStateException("L'adresse mail a déjà été confirmée.");
         }
 
         LocalDateTime expiredAt = confirmationToken.getExpiresAt();
 
+        // Control if token hasn't expired.
         if (expiredAt.isBefore(LocalDateTime.now())) {
             throw new IllegalStateException("Le token a expiré.");
         }
 
+        // Refresh token's informations with time activation.
         confirmationTokenService.setConfirmedAt(token);
+        // Activate account.
         userService.enableUser(
                 confirmationToken.getUser().getMail());
+        // Redirect user on login page.
         response.sendRedirect("http://localhost:8081/compte");
         return "Vous pouvez désormais vous connecter à notre plateforme discoodle ! :)";
     }
 
     private String buildMail(String name, String link) {
+        // It's mail pattern.
         return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
                 "\n" +
                 "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
